de935eeb055edc99db82ddb08d129bdf1193dacf
MikaelNineza <lnineza@ualberta.ca>
2026-09-13 22:21:59 -0600

    completed Unique Ownership section

--- src/UniquePtr.h
+++ src/UniquePtr.h
@@ -3,14 +3,64 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <utility>
 
 template <class T>
 class UniquePtr {
     public:
-        UniquePtr(T &p = nullptr) : mPtr {p}
+        template <class U> friend class UniquePtr;
+
+        UniquePtr(T* p = nullptr) : mPtr(p) {}
+
+        UniquePtr(const UniquePtr<T>& other) = delete;
+
+        UniquePtr(UniquePtr<T>&& other) {
+            mPtr = other.release();
+        }
+
+        template <class U>
+        UniquePtr(UniquePtr<U>&& other) {
+            mPtr = other.release();
+        }
+
         ~UniquePtr() {
             delete mPtr;
         }
+
+        T& operator*() const {return *mPtr;}
+
+        UniquePtr<T>& operator=(const UniquePtr<T>& other) = delete;
+
+        UniquePtr<T>& operator=(UniquePtr<T>&& other) {
+            if (this != &other) {
+                delete mPtr;
+                mPtr = other.release();
+            }
+            return *this;
+        }
+
+        T* operator->() const {return mPtr;}
+
+        T* get() const { return mPtr;}
+
+        bool operator==(const UniquePtr<T>& other) const {return mPtr == other.mPtr;}
+
+        T* release() {
+            T* p = mPtr;
+            mPtr = nullptr;
+            return p;
+        }
+
+        void reset(T* newPtr = nullptr) {
+            delete mPtr;
+            mPtr = newPtr;
+        }
+
+        void swap(UniquePtr<T>& other) {
+            std::swap(mPtr, other.mPtr);
+        }
+
+        operator bool() const {return mPtr != nullptr;}
 
     private:
         T* mPtr;
