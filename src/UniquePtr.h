8f5bff62887fd9a2152c125cf899b31dda875d14
MikaelNineza <lnineza@ualberta.ca>
2026-09-14 21:34:57 -0600

    completed makeUnique

--- src/UniquePtr.h
+++ src/UniquePtr.h
@@ -66,4 +66,11 @@ class UniquePtr {
         T* mPtr;
 };
 
+template <class T, class... Args>
+    UniquePtr<T> makeUnique(Args&&... args)
+    {
+        T* p = new T(std::forward<Args>(args)...);
+        return new UniquePtr(p);
+    }
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "UniquePtr.h"
 
 int main() {
-    // UniquePtr<int> ptr(new int(5));
+    UniquePtr<int> ptr(new int(5));
     // they test all methods here...
     return 0;
 }
